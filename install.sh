4195ec451a12a9f14a139c04d24fea0b9d907f03
MikimotoH <mikimotoh@gmail.com>
2013-10-19 17:58:29 +0800

    use hg clone to download mercurial-cli-template and hg-prompt

--- install.sh
+++ install.sh
@@ -2,25 +2,24 @@
 # pre-requirement:
 #   oh-my-zsh, vim 7.3(+python ) and clang installed
 
-function tool_installed(){
-    return hash $1 2>/dev/null
-}
+setenv PACKAGESITE http://freebsd.ntu.edu.tw/FreeBSD/ports/amd64/packages-9.2-release/Latest/
+export PACKAGESITE=http://freebsd.ntu.edu.tw/FreeBSD/ports/amd64/packages-9.2-release/Latest/
+[[ -d ~/.oh-my-zsh ]] || \
+    curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
 pkg_add -r wget
+pkg_add -r mercurial
+pkg_add -r vim
 
 if hash ack 2>/dev/null ; then
     echo "ack already installed"
 else
     echo "Install ack"
-    curl "http://beyondgrep.com/ack-2.04-single-file" > /usr/bin/ack && chmod 0755 /usr/bin/ack
+    curl "http://beyondgrep.com/ack-2.04-single-file" > /usr/local/bin/ack && chmod 0755 /usr/local/bin/ack
 fi
 
-if [ -d ~/.oh-my-zsh ]; then
-    echo "oh-my-zsh is already installed"
-else
-    echo "Install 'oh-my-zsh'"
-    curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-fi
+
+cp ./rkj-repos.zsh-theme ~/.oh-my-zsh/themes
 
 if [ -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
     echo "zsh-syntax-highlighting is already installed"
@@ -31,13 +30,16 @@ else
         git clone https://github.com/zsh-users/zsh-history-substring-search )
 fi
 
-ln -sf `readlink -f hg-prompt` ~/
+(cd ~ && hg clone https://bitbucket.org/sjl/hg-prompt )
+(cd ~ && hg clone https://bitbucket.org/sjl/mercurial-cli-templates)
+
 ln -sf `readlink -f .gitconfig` ~/
 ln -sf `readlink -f .vimrc` ~/
 ln -sf `readlink -f .vim` ~/
 ln -sf `readlink -f .zshrc` ~/
 ln -sf `readlink -f .hgrc`  ~/
 
+# Install VIM pathogen -- bundle manager
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl 'www.vim.org/scripts/download_script.php?src_id=19375' \
   > ~/.vim/autoload/pathogen.vim
@@ -55,7 +57,7 @@ if [ -d ~/.vim/bundle/nerdtree ]; then
     echo "NERDTree is already installed."
 else
     echo "Install NERDTree "
-    (cd ~/.vim/bundle; \
+    (cd ~/.vim/bundle && \
         git clone https://github.com/scrooloose/nerdtree.git)
 fi
 
@@ -63,7 +65,7 @@ if [ -d ~/.vim/bundle/tagbar ]; then
     echo "Tagbar is already installed."
 else
     echo "Install Tagbar "
-    (cd ~/.vim/bundle; \
+    (cd ~/.vim/bundle && \
         git clone git://github.com/majutsushi/tagbar)
 fi
 
@@ -71,17 +73,16 @@ if [ -f ~/.vim/plugin/ack.vim ]; then
     echo "ack.vim is already installed"
 else
     echo "Install ack.vim"
-    ( cd ~/.vim; curl -Sso ack.tar.gz \
-        "http://www.vim.org/scripts/download_script.php?src_id=10433";\
-        tar zxvf ack.tar.gz; rm ack.tar.gz )
+    ( cd ~/.vim && curl -Sso ack.tar.gz \
+        "http://www.vim.org/scripts/download_script.php?src_id=10433" \
+        && tar zxvf ack.tar.gz && rm ack.tar.gz )
 fi
-
 
 
 if [ -d ~/.vim/doc ] ; then
     if [ "$(ls -A ~/.vim/doc )" ]; then 
         print  "~/.vim/doc not empty" 
-        vim -c "helptags ~/.vim/doc/" -c q
+        vim -c "helptags ~/.vim/doc/" -c "q"
     else 
         print "~/.vim/doc is empty; don't helptags" 
     fi
